0b620d94409fb4577e5f1a5f6f659370dd32b679
Nilu Hilal <neelhil@yahoo.com>
2016-08-17 15:37:05 -0400

    stats.py

--- stats.py
+++ stats.py
@@ -51,8 +51,34 @@ print("tobacco mode {0}".format(stats.mode(df['Tobacco'])))
 """Measures of central tendency, variability of data"""
 """Range, variance and std of alcohol and tobacco"""
 
+"""alcohol:"""
+
 print("The range in alcohol data: {0}".format(max(df['Alcohol']) - min(df['Alcohol'])))
 
 print("The variance in alcohol data: {0}".format(df['Alcohol'].var()))
 
+print("The standard deviation in alcohol data: {0}".format(df['Alcohol'].var()))
+
+"""tobacco"""
+
+print("The range in tobacco data:{0}".format(max(df['Tobacco']) - min(df['Tobacco'])))
+
+print("The variance in tobacco data:{0}".format(df['Tobacco'].var()))
+
+print("The standard deviation in tobacco data:{0}".format(df['Tobacco'].std()))
+
+"""Ranking Data in distribution"""
+
+"""The citizens of Utopia work an average (mean)
+ of 251 days per year, with a standard deviation 
+ of 20 days. How many days correspond to a z-score
+ of 2.3?"""
+
+mean = 251
+std = 20
+z_score = 2.3
+
+x = mean + (z_score * std)
+
+print("Number of days that correspond to 2.3 z-score: {0}".format(x))
 
