a548c7c95e93d1c6b02c251f8b72c6178e095c4b
Nilu Hilal <neelhil@yahoo.com>
2016-09-10 16:00:31 -0400

    Cleanup with help

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,50 +3,44 @@
 import pandas as pd 
 import statsmodels.api as sm 
 
-loansData_clean = pd.read_csv('/Users/nilu057/thinkful/loansData_clean.csv')
+def clean_loan_data(dataframe):
+	dataframe['FICO.Score'] = dataframe['FICO.Range'].map(lambda x: x.split('-'))
+	dataframe['FICO.Score'] = dataframe['FICO.Score'].map(lambda x: int(min(x)))
+	fico = dataframe['FICO.Score']
+	intrt = dataframe['Interest.Rate'].map(lambda x: round(float(x.rstrip('%')) / 100, 2))
+	loanl = dataframe['Loan.Length'].map(lambda x: int(x.rstrip('months')))
+	dataframe['lower_rate'] = dataframe[intrt].map(lambda x: 1 if x < 0.12 else 0)
+	return dataframe
 
-loansData_clean['FICO.Score'] = loansData_clean['FICO.Range'].map(lambda x: x.split('-'))
+def logistic_regression(cleaned_dataframe):
+	cleaned_dataframe['intercept'] = 1.0
+	ind_vars = cleaned_dataframe[loanl, fico]
 
-loansData_clean['FICO.Score'] = loansData_clean['FICO.Score'].map(lambda x: int(min(x)))
+	"""Logistic Regression Model"""
+	logit = sm.Logit(cleaned_dataframe['lower_rate'], cleaned_dataframe[ind_vars])
 
-fico = loansData_clean['FICO.Score']
+	"""fitting the model"""
+	result = logit.fit()
 
-intrt = loansData_clean['Interest.Rate'].map(lambda x: round(float(x.rstrip('%')) / 100, 2))
-
-loanl = loansData_clean['Loan.Length'].map(lambda x: int(x.rstrip('months')))
-
-loansData_clean['lower_rate'] = loansData_clean[intrt].map(lambda x: 1 if x < 0.12 else 0)
-
-print(loansData_clean['lower_rate'])
-
-loansData_clean['intercept'] = 1.0
-
-ind_vars = loansData_clean[loanl, fico]
-
-'''print(loansData_clean[loansData_clean['Interest.Rate'] == 0.10].head())
-print(loansData_clean[loansData_clean['Interest.Rate'] == 0.13].head())'''
-
-"""Logistic Regression Model"""
-
-logit = sm.Logit(loansData_clean['lower_rate'], loansData_clean[ind_vars])
-
-"""fitting the model"""
-
-result = logit.fit()
-
-"""fitted coefficients"""
-coeff = result.params
-print(coeff)
-
-"""linear prediction"""
-''''interest_rate = -60.125 + 0.087423(FicoScore) - 0.000174(LoanAmount)'''
-
-"""logistic function"""
+	"""linear prediction"""
+	''''interest_rate = -60.125 + 0.087423(FicoScore) - 0.000174(LoanAmount)'''
+	return result
 
 
+def main():
+	rawDataframe = pd.read_csv('/Users/nilu057/thinkful/dataframe.csv')
+	cleanedDataframe = clean_loan_data(rawDataframe)
+	print("Cleaned datas lower_rate %s " % dataframe['lower_rate'])
+	result = logistic_regression(cleanedDataframe)
+	# fitted coefficients	
+	print("Logistic Regression coefficent: %s" % result.params)
+
+if __name__ == '__main__':
+	main()
 
 
-
+'''print(dataframe[dataframe['Interest.Rate'] == 0.10].head())
+print(dataframe[dataframe['Interest.Rate'] == 0.13].head())'''
 
 
 
